b1f8a5daa568531dfb498106fa7d4cfe8b36dd48
zhanghollow <hollowzheng@gmail.com>
2024-04-26 17:13:44 +0800

    feat: 优化ts文件无法引入图片格式等文件

--- src/views/Test/test4/index.tsx
+++ src/views/Test/test4/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from 'react'
+import './index.sass'
 
-const Test: React.FC = () => {
-  return <div>Hello World!</div>;
-};
+// Card-practice
+const Test: React.FC<any> = (props) => {
+	const { title, cover, extra, loading, children, ...restProps } = props || {}
+	let head: React.ReactNode
+	if (title || extra) {
+		head = (
+			<div className="Card-head">
+				{title && <div>{title}</div>}
+				{extra && <div>{extra}</div>}
+			</div>
+		)
+	}
 
-export default Test;
+	const coverDom = cover && <div className='Card-cover'>{loading ? null : cover}</div>
+
+	const body = children && (
+		<div className="Card-body">{loading ? null : children}</div>
+	)
+
+	return (
+		<>
+			<div className="Card">
+				{head}
+				{coverDom}
+				{body}
+			</div>
+		</>
+	)
+}
+
+export default Test
