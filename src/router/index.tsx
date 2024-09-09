1ffbcd25056d1e9b568e21f8ea326192cea9d6e8
hollowky <hollowzheng@gmail.com>
2024-09-09 23:34:25 +0800

    feat:代码提交

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Default from '@/views/Default/index'
 import Canvas from '@/views/Canvas/index'
 import ICss from '@/views/ICss/index'
+import Appraisal from '@/views/Appraisal_System_Project/index'
 
 const Routers = () => {
 	return (
@@ -11,6 +12,7 @@ const Routers = () => {
 				<Route path="/" element={<Default />}></Route>
 				<Route path="/canvas" element={<Canvas />}></Route>
 				<Route path="/icss" element={<ICss />}></Route>
+				<Route path="/app" element={<Appraisal />}></Route>
 			</Routes>
 		</BrowserRouter>
 	)
