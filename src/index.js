59c068caaa38d117bf62cc0bea02c9b73f82d558
Zav Zhang <hollowzheng@gmail.com>
2024-12-16 20:33:25 +0800

    提交分支1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Routers from './router/index'
 
+console.log('this is new test1');
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Routers />)
